f2605748d91ec9be0ceca3e204f8d75398d96757
Mark70092 <114631937+Mark70092@users.noreply.github.com>
2024-06-06 16:53:23 -0400

    Update home_page.py

--- page/home_page.py
+++ page/home_page.py
@@ -1,1 +1,14 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+
+class HomePage(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.search_box = (By.NAME, 'search_query')
+        self.search_button = (By.ID, 'search-icon-legacy')
+
+    def search(self, text):
+        search_input = self.find_element(self.search_box)
+        search_input.send_keys(text)
+        self.click(self.search_button)
 
