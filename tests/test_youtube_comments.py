a55a7e75a6716e0959b951e17f94d5a7ef04b908
Mark70092 <114631937+Mark70092@users.noreply.github.com>
2024-06-06 17:13:38 -0400

    Create test_youtube_comments.py

---
+++ tests/test_youtube_comments.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from utils.browser import get_driver
+
+@pytest.fixture
+def driver():
+    driver = get_driver()
+    yield driver
+    driver.quit()
+
+def test_youtube_comments(driver):
+    video_url = "https://www.youtube.com/watch?v=Xjv1sY630Uc&list=PLzMcBGfZo4-n40rB1XaJ0ak1bemvlqumQ&ab_channel=TechWithTim"
+    driver.get(video_url)
+    
+    # Ожидание загрузки комментариев
+    driver.implicitly_wait(10)
+    
+    # Проверка наличия секции комментариев
+    comments_section = driver.find_element_by_xpath('//*[@id="comments"]')
+    assert comments_section is not None
